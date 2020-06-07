6a0d1d76d3e1b265de3165930d071aebb59cb970
Orlando G Morales <chorly786@gmail.com>
2020-06-07 16:42:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
 
+<<<<<<< HEAD
 var greetSpecialGuest = `Hello, ${specialGuest}!`;
+=======
+var greetSpecialGuest = "Hello" + specialGuest + "!";
+>>>>>>> 2fb218eee8861a0f23b5325f3748b869f2b4f24f
 
 var topic = 'space';
 
